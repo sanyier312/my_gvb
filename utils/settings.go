8df81f3de5388f9f2ee7de65690fd1499fcb1f42
林夕 <127731781+sanyier312@users.noreply.github.com>
2023-05-10 20:50:44 +0800

    前端admin打包

--- utils/settings.go
+++ utils/settings.go
@@ -24,6 +24,7 @@ var (
 	QiniuSever string
 )
 
+// init 读取配置文件
 func init() {
 	file, err := ini.Load("config/config.ini")
 	if err != nil {
@@ -35,12 +36,14 @@ func init() {
 
 }
 
+// LoadServer 读取server配置
 func LoadServer(file *ini.File) {
 	AppMode = file.Section("server").Key("AppMode").MustString("debug")
 	HttpPort = file.Section("server").Key("HttpPort").MustString(":3000")
 
 }
 
+// LoadData 读取数据库配置
 func LoadData(file *ini.File) {
 
 	DbHost = file.Section("database").Key("DbHost").MustString("localhost")
@@ -49,6 +52,8 @@ func LoadData(file *ini.File) {
 	DbPassWord = file.Section("database").Key("DbPassWord").String()
 	DbName = file.Section("database").Key("DbName").MustString("gvb_db")
 }
+
+// LoadQiniu 读取七牛云配置
 func LoadQiniu(file *ini.File) {
 	Zone = file.Section("qiniu").Key("Zone").MustInt(1)
 	AccessKey = file.Section("qiniu").Key("AccessKey").String()
